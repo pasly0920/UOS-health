c338f0d267ca3e2d7c81d168cecf49a463858545
pasly0920 <inwoo920621@gmail.com>
2021-11-27 22:11:09 +0900

    원형 chart

--- js/dietChart.js
+++ js/dietChart.js
@@ -1,6 +1,6 @@
 const colorFn = (i, classname, color) => {
-    classname.style.background = "conic-gradient(" + color + "0%" + i + "%, #dedede" + i + "% 100%)";
-}
+    classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #dedede " + i + "% 100%)";
+  }
 
 const makeChart = (percent, classname, color) => {
     let i = 1;
@@ -18,9 +18,15 @@ document.addEventListener("DOMContentLoaded", function(){
     const chart2 = document.querySelector('.carbo');
     const chart3 = document.querySelector('.fat');
 
-    const chart4 = document.querySelector('sweet');
-    const chart5 = document.querySelector('natrium');
+    const chart4 = document.querySelector('.sweet');
+    const chart5 = document.querySelector('.natrium');
+    const chart6 = document.querySelector('.calrorie');
 
-    makeChart(80, chart1, '#66d2ce');
+    makeChart(80, chart1, '#ffafb0');
+    makeChart(50, chart2, '#b5c7ed');
+    makeChart(50, chart3, '#ffe4af');
+    makeChart(80, chart4, '#afffba');
+    makeChart(80, chart5, '#fdfa87');
+    makeChart(80, chart6, 'aquamarine');
 })
 
